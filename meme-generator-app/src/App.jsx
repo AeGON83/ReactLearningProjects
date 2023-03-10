b7227e14b32f9b66f11dfaddd099b0f901f776e5
RajansinhMakwana <AeGON83@users.noreply.github.com>
2023-03-10 11:34:11 +0530

    Day 10

--- meme-generator-app/src/App.jsx
+++ meme-generator-app/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Generator from './components/Generator'
 import data from './components/data'
+import ImageIndex from './components/ImageIndex'
 
 function App() {
 
   return (
-    <Generator data={data} />
+    <div className='main'>
+      <Generator data={data} />
+      <ImageIndex data={data} />
+    </div>
   )
 }
 
